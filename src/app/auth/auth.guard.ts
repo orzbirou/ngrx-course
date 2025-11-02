770b8fecdc9d661286cb1d4a122dc545100dc1a8
orzbirou <orzbirou@users.noreply.github.com>
2025-11-03 00:51:03 +0200

    feat: add AuthGuard for route protection
    - Implemented CanActivate guard using NgRx store
    - Added automatic redirect to login for unauthenticated users
    - Used isLoggedIn selector for reactive authentication checks
    - Protects routes requiring authentication

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,22 @@
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
+import { Store, select } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
+import { AppState } from "../reducers";
+import { isLoggedIn } from "./auth.selectors";
+import { Injectable } from "@angular/core";
+
+@Injectable  ()
+export class AuthGuard implements CanActivate {
+    constructor(private store: Store<AppState>, private router: Router) {}
+    canActivate( route: ActivatedRouteSnapshot, state: RouterStateSnapshot ): Observable<boolean> {
+        return this.store.pipe(
+            select(isLoggedIn),
+            tap(loggedIn => {
+                if (!loggedIn) {
+                    this.router.navigateByUrl('/login');
+                }
+            })
+        );
+    }
+};
