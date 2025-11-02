96b05eb4a01b06cb030215aa585f7d7a642c6e62
orzbirou <orzbirou@users.noreply.github.com>
2025-11-03 00:51:55 +0200

    feat: enhance AppModule with advanced NgRx configuration
    - Added EffectsModule.forRoot([]) for global effects
    - Integrated StoreRouterConnectingModule for router state sync
    - Added strict runtime checks for state and action immutability/serializability
    - Protected courses route with AuthGuard
    - Improved code formatting and organization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,57 +1,78 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent } from './app.component';
+import { MatIconModule } from "@angular/material/icon";
+import { MatMenuModule } from "@angular/material/menu";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppComponent } from "./app.component";
 
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from "@angular/common/http";
+import { MatListModule } from "@angular/material/list";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
-import { RouterModule, Routes } from '@angular/router';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { AuthModule } from './auth/auth.module';
+import { RouterModule, Routes } from "@angular/router";
+import { RouterState } from "@ngrx/router-store";
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { AuthModule } from "./auth/auth.module";
 
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { environment } from '../environments/environment';
-import { metaReducers, reducers } from './reducers';
-
-
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { environment } from "../environments/environment";
+import { metaReducers, reducers } from "./reducers";
+import { AuthGuard } from "./auth/auth.guard";
+import { EffectsModule } from "@ngrx/effects";
+import { StoreRouterConnectingModule } from "@ngrx/router-store";
 
 const routes: Routes = [
   {
-    path: 'courses',
-    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+    path: "courses",
+    loadChildren: () =>
+      import("./courses/courses.module").then((m) => m.CoursesModule),
+    canActivate: [AuthGuard],
   },
   {
-    path: '**',
-    redirectTo: '/'
-  }
+    path: "**",
+    redirectTo: "/",
+  },
 ];
 
-
-
-@NgModule({ declarations: [
-        AppComponent
-    ],
-    bootstrap: [AppComponent], 
-    providers: [provideHttpClient(withInterceptorsFromDi())],
-    imports: [BrowserModule,
-        BrowserAnimationsModule,
-        RouterModule.forRoot(routes),
-        MatMenuModule,
-        MatIconModule,
-        MatSidenavModule,
-        MatProgressSpinnerModule,
-        MatListModule,
-        MatToolbarModule,
-        AuthModule.forRoot(),
-        StoreModule.forRoot(reducers, {metaReducers}),
-        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })]
-      })
-export class AppModule {
-}
+@NgModule({
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    MatMenuModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatProgressSpinnerModule,
+    MatListModule,
+    MatToolbarModule,
+    AuthModule.forRoot(),
+    StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true,
+        strictStateSerializability: true,
+        strictActionSerializability: true
+      }
+    }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot({
+      stateKey: "router",
+      routerState: RouterState.Minimal,
+    }),
+  ],
+})
+export class AppModule {}
